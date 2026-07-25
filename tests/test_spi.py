c8edacb870f206fbfc062d5b38a8206caeba2311
cmz488 <3425486153@qq.com>
2026-07-25 18:50:35 +0800

    test_spi

--- tests/test_spi.py
+++ tests/test_spi.py
@@ -1,6 +1,24 @@
+import time
 from periphery import SPI
 
+spi = SPI(
+    devpath="/dev/spidev0.0",
+    mode=0,
+    max_speed=32_000_000,
+)
 
-spi = SPI(devpath="/dev/spidev0.0", mode=0, max_speed=32000000)
-while True:
-    print(spi.transfer(bytearray(b"hello")))
+# 当前下位机协议要求固定发送16字节
+tx_data = bytearray(16)
+tx_data[0:5] = b"hello"
+
+try:
+    while True:
+        rx_data = bytes(spi.transfer(tx_data))
+
+        print("TX:", tx_data.hex(" "))
+        print("RX:", rx_data.hex(" "))
+
+        # 当前下位机在主循环中重新装载DMA，需要帧间隔
+        time.sleep(0.001)
+finally:
+    spi.close()
